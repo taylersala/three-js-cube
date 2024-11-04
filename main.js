9fa1e5e45e87d6eeda33c2ed1d0fc9e25545dd1c
Tayler Sala <119762971+taylersala@users.noreply.github.com>
2024-11-04 14:14:13 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+
+//Create the Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// ADD THE CAMERA takes pov, ratio, near plane, far plane
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+//create and add a cube object
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//add lighting
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1, 1, 1); //x y z axis
+scene.add(light);
+
+//Set up renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Animate the scene
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();
